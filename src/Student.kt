753e836c56c1e5efc3479245e5ad25ed0c5d0744
2brooj75 <2brooj75@solent.ac.uk>
2025-09-26 10:57:48 +0100

    feat: Created different types of Student class: concise version, init block version and a potential dataclass varient

--- src/Student.kt
+++ src/Student.kt
@@ -1,19 +1,36 @@
-class Student(var idIn: String, var nameIn: String, var courseIn: String, var markIn: Double) {
+// Concise Version
 
-    val id: String
-    val name: String
-    val course: String
-    val mark: Double
+//class Student(val id: String, var name: String, var course: String, var mark: Double) {
+//
+//    override fun toString(): String {
+//        return "Student(name='${this.name}', course='${this.course}' mark='${this.mark}')"
+//    }
+//
+//}
 
-    init {
-        this.id = idIn
-        this.name = nameIn
-        this.course = courseIn
-        this.mark = markIn
-    }
+// Init Block Version
+//
+//class Student(id: String, name: String, course: String, mark: Double) {
+//    var id: String = id
+//    var name: String = name
+//    var course: String = course
+//    var mark: Double = mark
+//
+//    init {
+//        this.id = id
+//        this.name = name
+//        this.course = course
+//        this.mark = mark
+//    }
+//
+//    override fun toString(): String {
+//        return "Student(id='${this.id}', name='${this.name}', course='${this.course}', mark=${this.mark})"
+//    }
+//}
 
+//Dataclass
+data class Student(val id: String, var name: String, var course: String, var mark: Double) {
     override fun toString(): String {
-        return "Student(name='${this.name}', course='${this.course}' mark='${this.mark}')"
+        return "Student(id='${this.id}', name='${this.name}', course='${this.course}', mark=${this.mark})"
     }
-
 }
