47bc92b69e088c37a75e1c0278cd76f46a63a195
Microsft-Wrd <mrjacobbrooks@gmail.com>
2025-09-29 13:50:59 +0100

    feat: Added Grade Checker and mark setter

--- src/Student.kt
+++ src/Student.kt
@@ -10,27 +10,40 @@
 
 // Init Block Version
 //
-//class Student(id: String, name: String, course: String, mark: Double) {
-//    var id: String = id
-//    var name: String = name
-//    var course: String = course
-//    var mark: Double = mark
-//
-//    init {
-//        this.id = id
-//        this.name = name
-//        this.course = course
-//        this.mark = mark
-//    }
-//
-//    override fun toString(): String {
-//        return "Student(id='${this.id}', name='${this.name}', course='${this.course}', mark=${this.mark})"
-//    }
-//}
+class Student(id: String, name: String, course: String, mark: Int) {
+    var id: String = id
+    var name: String = name
+    var course: String = course
+    var mark: Int = 0
+        set(value) {
+            field = if(value in 1..100) value else return
+        }
 
-//Dataclass
-data class Student(val id: String, var name: String, var course: String, var mark: Double) {
+    init {
+        this.id = id
+        this.name = name
+        this.course = course
+        this.mark = mark
+    }
+
+    fun getGrade(): String {
+        return when {
+            this.mark > 70 -> "First"
+            this.mark in 60 .. 69 -> "2/1"
+            this.mark in 50 .. 59 -> "2/2"
+            this.mark in 40 .. 49 -> "Third"
+            else -> "Fail"
+        }
+    }
+
     override fun toString(): String {
         return "Student(id='${this.id}', name='${this.name}', course='${this.course}', mark=${this.mark})"
     }
 }
+
+//Dataclass
+//data class Student(val id: String, var name: String, var course: String, var mark: Double) {
+//    override fun toString(): String {
+//        return "Student(id='${this.id}', name='${this.name}', course='${this.course}', mark=${this.mark})"
+//    }
+//}
